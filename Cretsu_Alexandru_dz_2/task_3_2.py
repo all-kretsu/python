aa82ab05f38854043e31af59b26ad6031cb4305c
all-kretsu <84284416+all-kretsu@users.noreply.github.com>
2022-03-24 14:27:44 +0300

    Update task_3_2.py

--- Cretsu_Alexandru_dz_2/task_3_2.py
+++ Cretsu_Alexandru_dz_2/task_3_2.py
@@ -2,10 +2,12 @@
 worker_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА',
                'токарь высшего разряда нИКОЛАЙ', 'директор аэлита']
 
-
+names = []
 for words in worker_list:
-    for letter in words:
-        if letter != ' ':
-            print(letter[-1:])
-        else:
-            pass
+    for letters in words:
+        
+    words = words.lower()
+    if 'игорь' or 'марина' or 'николай' in words:
+        
+      
+      
