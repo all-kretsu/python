520a05cadd64fde8301a6a9b2d0ed3dacae933f1
Alexandru <all-kretsu@yandex.ru>
2022-10-30 20:04:27 +0300

    task_1_3
    Task contain dict with number-translate string and funtion which checks the presence of a word in dict.

---
+++ Cretsu_Alexandru_dz_3/task_1_3.py
@@ -0,0 +1,28 @@
+# Задание 1, урок 3, перевод слов.
+
+numbers = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три',
+           'four': 'четыре', 'five': 'пять', 'six': 'шесть', 'seven': 'семь',
+           'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+
+
+# numb = input('Insert here: ')
+
+
+# Фунция перевода входящих значений
+def num_translate(word):
+    for eng in numbers:
+        if word == eng:
+            print(numbers.setdefault(eng))
+
+
+# num_translate(numb)
+
+
+# Дополненная функция с возвратом заглавной буквы
+def num_translate_adv(word):
+    num_translate(word)
+    for eng in numbers:
+        if word == eng.capitalize():
+            print(numbers.setdefault(eng).capitalize())
+
+
