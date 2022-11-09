c44e040deb1255a3347d31581afff4b2216853b8
Alexandru <all-kretsu@yandex.ru>
2022-11-09 21:07:41 +0300

    Lesson 3, task 5: task modified

--- Cretsu_Alexandru_dz_3/task_5_3.py
+++ Cretsu_Alexandru_dz_3/task_5_3.py
@@ -1,11 +1,20 @@
+import random
+
 # Урок 3, задание 5, создание генератора шуток.
 
+
+# списки со словами
 nouns = ['автомобиль', 'лес', 'огонь', 'город', 'дом']
 verbs = ['сегодня', 'вчера', 'завтра', 'позавчера', 'ночью']
 adjectives = ['веселый', 'мягкий', 'зеленый', 'утопичный', 'яркий']
 
-def get_jokes(*args):
-    print(args)
 
+# функция с совмещением списков в предложения
+def get_jokes(n, v, a):
+    random.shuffle(n)
+    random.shuffle(v)
+    random.shuffle(a)
+    zip_words = list(zip(n, v, a))
+    print(zip_words)
 
-get_jokes(nouns, verbs, adjectives)g
+get_jokes(nouns, verbs, adjectives)
