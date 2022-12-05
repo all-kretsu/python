d963e6886a4788b5b2fb5cb602d6b52a8d605cff
Alexandru <all-kretsu@yandex.ru>
2022-12-05 21:35:57 +0300

    Lesson 4, task 2; Курс валют Создана часть скрипта по выводу данных из ресурса в список и выведения элемента списка под запрашиваемую валюту.

--- env4/Cretsu_Alexandru_dz_4/task_2_4.py
+++ env4/Cretsu_Alexandru_dz_4/task_2_4.py
@@ -1,14 +1,21 @@
+"""Этот модуль выводит курс обозначенной валюты"""
+
+
 import requests
 
 
 def currency_rate(currency):
+    # get http
     cur_rate = requests.get('http://cbr.ru/scripts/XML_daily.asp').text
+    # split in list result of http
     rate_lst = list(cur_rate.split('ID'))
-    print(rate_lst)
-    for i in rate_lst:
-        numb = [float(x) for x in i.split() if x.isdigit()]
-        if currency in i:
-            print(numb)
+    # get requested value
+    numb = [x.split('Value') for x in rate_lst if currency in x]
+    # split requested value
+    for y in numb:
+
+        print(y[-2])
+    # print(''.join(numb))
 
 
 currency_rate('MDL')
